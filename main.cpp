7d81e5711b3232aa0bf88f023bebdfc0b7033e80
cinwell <gongxinwei@gmail.com>
2018-04-08 16:25:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,16 +20,31 @@ using namespace cv;
 int main(int argc, char* argv[])
 {
 	
-	Mat img = imread("6.png");
+	string filename = argv[1];
+	float thr = 100;
+	if (argc > 2)
+		thr = atof(argv[2]);
+	Mat img = imread(filename);
+
 	PreGraph SpMat;
 	Mat superpixels = SpMat.GeneSp(img);
 	Mat sal = SpMat.GeneSal(img);
-	cout << sal;
+	//cout << sal;
 	Mat salMap = SpMat.Sal2Img(superpixels, sal);
+	/*
 	Mat tmpsuperpixels;
 	normalize(salMap, tmpsuperpixels, 255.0, 0.0, NORM_MINMAX);
 	tmpsuperpixels.convertTo(tmpsuperpixels, CV_8UC3, 1.0);
 	imshow("sp", tmpsuperpixels);
 	waitKey();
+	*/
+	Mat mask;
+	normalize(salMap, mask, 255.0, 0.0, NORM_MINMAX);
+	mask.convertTo(mask, CV_8UC3, 1.0);
+	mask = mask > thr;
+	Mat img1 = cv::Mat::zeros(img.size(), CV_8UC3);
+	img.copyTo(img1, mask);
+	imshow("sp", img1);
+
 	return 0;
 }
